930913c0deb7b67dc8e1934a60b98bcc309a63f1
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-07-18 21:22:14 +0300

    Улучшаем версию для слабовидящих. Теперь куки работают в режиме host-only (без распространения на поддомены). Исправлены стили при активации версии для слабовидящих

--- components/bvi.version/templates/.default/dist/js/bvi.js
+++ components/bvi.version/templates/.default/dist/js/bvi.js
@@ -1037,7 +1037,7 @@
     var time = now.getTime();
     time += 24 * 60 * 60 * 1000;
     now.setTime(time);
-    document.cookie = "bvi_".concat(name, "=").concat(value, ";path=/;expires=").concat(now.toUTCString(), ";domain=").concat(location.host);
+    document.cookie = "bvi_".concat(name, "=").concat(value, ";path=/;expires=").concat(now.toUTCString());
   };
 
   var getCookie = function getCookie() {
@@ -1057,7 +1057,7 @@
 
   var removeCookie = function removeCookie() {
     var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-    document.cookie = "bvi_".concat(name, "=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=").concat(location.host);
+    document.cookie = "bvi_".concat(name, "=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT");
   };
 
   /**
