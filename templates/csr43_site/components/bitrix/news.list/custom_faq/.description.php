39e705d273f4a816825fdb097b20056f5e152bdb
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-07-19 22:18:32 +0300

    Добавление описаний для новых компонентов: маршруты общественного транспорта, материалы общественных обсуждений и часто задаваемые вопросы

---
+++ templates/csr43_site/components/bitrix/news.list/custom_faq/.description.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arTemplate = [
+    'NAME' => 'Часто задаваемые вопросы',
+    'DESCRIPTION' => 'Выводит список вопросов и ответов в виде адаптивного аккордеона.',
+];
