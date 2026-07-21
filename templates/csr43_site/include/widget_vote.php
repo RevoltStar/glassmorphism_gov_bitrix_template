eae99407a64bd7abbd83f11f3ecf5e75122e9c8a
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-07-21 07:46:31 +0300

    Откат изменений для виджета госуслуг и виджета для голосований

--- templates/csr43_site/include/widget_vote.php
+++ templates/csr43_site/include/widget_vote.php
@@ -5,5 +5,5 @@ if ($gosuslugiOrgId === 0) {
     return;
 }
 ?>
-<div id="e329fb40-widget-pos" class="external-vote-widget" style="height: 100%; border: 1px solid gray;"></div>
+<div id="e329fb40-widget-pos" style="height: 100%; border: 1px solid gray;"></div>
 <script id="e329fb40" src="https://pos.gosuslugi.ru/og/widget/js/main.js" data-src-host="https://pos.gosuslugi.ru/og" data-org-id="<?=$gosuslugiOrgId?>"></script>
