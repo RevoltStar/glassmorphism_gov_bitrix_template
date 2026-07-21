a7fe8fc8707c89075fac6c6678815fe7fabbb86d
RevoltStar <poroshinmaksimigorevich@gmail.com>
2026-07-21 14:53:25 +0300

    Добавляем кеширование для меню social в футере

--- templates/csr43_site/footer.php
+++ templates/csr43_site/footer.php
@@ -64,7 +64,7 @@ if (!$isExcludedPage && !$is404Error): ?>
 							"USE_EXT" => "N",
 							"DELAY" => "N",
 							"ALLOW_MULTI_SELECT" => "N",
-							"MENU_CACHE_TYPE" => "N",
+							"MENU_CACHE_TYPE" => "A",
 							"MENU_CACHE_TIME" => "3600",
 							"MENU_CACHE_USE_GROUPS" => "Y",
 							"MENU_CACHE_GET_VARS" => ""
